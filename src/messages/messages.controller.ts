000d0e35447f302266ad96520f6761b02c391ffe
Yannick Bley <bleyyannickpro@gmail.com>
2024-03-04 17:06:58 +0100

    learn: DI and modules with NestJs

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,15 +1,23 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from 'src/dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  constructor(public messagesService: MessagesService) {}
+
+  @Get()
+  listMessages() {
+    this.messagesService.findAll();
+  }
+
   @Post()
-  createMessages(@Body() body: CreateMessageDto) {
-    console.log(body);
+  createMessage(@Body() body: CreateMessageDto) {
+    this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param() id: string) {
-    console.log(id);
+    this.messagesService.findOne(id);
   }
 }
